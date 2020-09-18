36f5f9f6d5c56ba9836a40f49edbb260254db0c7
Christian Schmidt <christian@a26z.de>
2020-09-18 18:05:35 +0200

    first draft

--- server.js
+++ server.js
@@ -1,24 +1,46 @@
 'use strict';
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-var express = require('express');
-var cors = require('cors');
-var multer = require('multer');
+const cors = require('cors');
+const multer = require('multer');
 
-// require and use "multer"...
+const PORT = process.env.PORT || 3000;
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
 
-var app = express();
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/', function(req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.get('/hello', function(req, res) {
+    res.json({
+        greetings: "Hello, API"
+    });
+});
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, `${process.cwd()}/storage`)
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${file.fieldname}-${Date().toString()}`)
+    }
+});
+const upload = multer({
+    storage: storage
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+    const file = req.file;
+    res.json(file);
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log(`Node.js listening on port ${PORT}, -----> ${BASE_URL}`);
 });
